006e68cdd864fb8c67b30ecea2767d0178c8c269
alvnz11 <alvanzasaputra123734@gmail.com>
2024-02-19 10:34:29 +0700

    update percobaan 3

--- Pertemuan2/jobsheet_2/Buku04.java
+++ Pertemuan2/jobsheet_2/Buku04.java
@@ -4,6 +4,18 @@ public class Buku04 {
     String judul, pengarang;
     int halaman, stok, harga;
 
+    public Buku04() {
+
+    }
+
+    public Buku04(String jud, String pg, int hal, int stok, int har) {
+        judul = jud;
+        pengarang = pg;
+        halaman = hal;
+        this.stok = stok;
+        harga = har;
+    }
+
     void tampilInformasi() {
         System.out.println("Judul : " + judul);
         System.out.println("Pengarang : " + pengarang);
--- Pertemuan2/jobsheet_2/BukuMain04.java
+++ Pertemuan2/jobsheet_2/BukuMain04.java
@@ -3,8 +3,10 @@ package Pertemuan2.jobsheet_2;
 public class BukuMain04 {
     public static void main(String[] args) {
         
-        Buku04 bk1 = new Buku04();
+        Buku04 bk1 = new Buku04(); // instansiasi tanpa konstruktor
+        Buku04 bk2 = new Buku04("Self Reward", "Maheera Ayesha", 160, 29, 59000); // instansiasi dengan konstruktor
 
+        // tanpa konstruktor
         bk1.judul = "Today Ends Tomorrow Comes";
         bk1.pengarang = "Denanda Pratiwi";
         bk1.halaman = 198;
@@ -15,5 +17,9 @@ public class BukuMain04 {
         bk1.terjual(5);
         bk1.gantiHarga(60000);
         bk1.tampilInformasi();
+
+        // dengan konstruktor
+        bk2.terjual(11);
+        bk2.tampilInformasi();
     }
 }
