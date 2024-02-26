7ef076c645a10bccaa65f2f4b1cf294ab9433fa9
alvnz11 <alvanzasaputra123734@gmail.com>
2024-02-26 09:38:02 +0700

    praktikum1

---
+++ Pertemuan3/josheet_3/persegiPanjang.java
@@ -0,0 +1,26 @@
+package Pertemuan3.josheet_3;
+
+public class persegiPanjang {
+    public int panjang;
+    public int lebar;
+
+    public static void main(String[] args) {
+        persegiPanjang[] ppArray = new persegiPanjang[3];
+
+        ppArray[0] = new persegiPanjang();
+        ppArray[0].panjang = 110;
+        ppArray[0].lebar = 30;
+
+        ppArray[1] = new persegiPanjang();
+        ppArray[1].panjang = 60;
+        ppArray[1].lebar = 40;
+
+        ppArray[2] = new persegiPanjang();
+        ppArray[2].panjang = 100;
+        ppArray[2].lebar = 20;
+
+        System.out.println("Persegi panjang ke-0, panjang: " + ppArray[0].panjang + ", lebar: " + ppArray[0].lebar);
+        System.out.println("Persegi panjang ke-1, panjang: " + ppArray[1].panjang + ", lebar: " + ppArray[1].lebar);
+        System.out.println("Persegi panjang ke-2, panjang: " + ppArray[2].panjang + ", lebar: " + ppArray[2].lebar);
+    }
+}
