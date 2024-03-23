400bf498ef10b61428e3b907e9d8e69a2ee6de67
alvnz11 <you@example.com>
2024-03-23 12:16:09 +0700

    percobaan 1

---
+++ Pertemuan5/jobsheet_4/BruteForceDivideConquer/minggu5/Faktorial/src/MainFaktorial.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class MainFaktorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--------------------------------");
+        System.out.print("Masukkan jumlah elemen: ");
+        int iJml = sc.nextInt();
+
+        Faktorial[] fk = new Faktorial[iJml];
+        for(int i = 0; i < iJml; i++) {
+            fk[i] = new Faktorial();
+            System.out.print("Masukkan nilai data ke-" + (i+1) + ": ");
+            int iNilai = sc.nextInt();
+            fk[i].nilai = iNilai;
+        }
+
+        // brute force
+        System.out.println("Hasil - Brute Force");
+        for(int i = 0; i < iJml; i++) {
+            System.out.println("Hasil penghitungan faktorial menggunakan Brute Force adalah " + fk[i].faktorialBF(fk[i].nilai));
+        }
+
+        // divide conquer
+        System.out.println("Hasil - Divide Conquer");
+        for(int i = 0; i < iJml; i++) {
+            System.out.println("Hasil penghitungan faktorial menggunakan Divide Conquer adalah " + fk[i].faktorialDC(fk[i].nilai));
+        }
+
+    }
+}
