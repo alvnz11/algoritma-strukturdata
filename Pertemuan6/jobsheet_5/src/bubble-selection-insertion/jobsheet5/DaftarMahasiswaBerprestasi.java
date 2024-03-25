6ffef93ee358f5c43826fac727f683d441eef8c6
alvnz11 <you@example.com>
2024-03-25 10:04:45 +0700

    Percobaan 1

---
+++ Pertemuan6/jobsheet_5/src/bubble-selection-insertion/jobsheet5/DaftarMahasiswaBerprestasi.java
@@ -0,0 +1,32 @@
+public class DaftarMahasiswaBerprestasi {
+    Mahasiswa listMhs[] = new Mahasiswa[5];
+    int idx;
+
+    void tambah(Mahasiswa m) {
+        if(idx < listMhs.length) {
+            listMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("Data Sudah Penuh!!");
+        }
+    }
+
+    void tampil() {
+        for(Mahasiswa m : listMhs) {
+            m.tampil();
+            System.out.println("-----------------------");
+        }
+    }
+
+    void bubblesort() {
+        for(int i = 0; i < listMhs.length-1; i++) {
+            for(int j = 1; j < listMhs.length-i; j++) {
+                if(listMhs[j].ipk > listMhs[j-1].ipk) {
+                    Mahasiswa tmp = listMhs[j];
+                    listMhs[j] = listMhs[j-1];
+                    listMhs[j-1] = tmp;
+                }
+            }
+        }
+    }
+}
---
+++ Pertemuan6/jobsheet_5/src/bubble-selection-insertion/jobsheet5/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        DaftarMahasiswaBerprestasi list = new DaftarMahasiswaBerprestasi();
+        Mahasiswa m1 = new Mahasiswa("Nusa", 2017, 25, 3);
+        Mahasiswa m2 = new Mahasiswa("Rara", 2012, 19, 4);
+        Mahasiswa m3 = new Mahasiswa("Dompu", 2018, 19, 3.5);
+        Mahasiswa m4 = new Mahasiswa("Abdul", 2017, 23, 2);
+        Mahasiswa m5 = new Mahasiswa("Ummi", 2019, 21, 3.75);
+
+        list.tambah(m1);
+        list.tambah(m2);
+        list.tambah(m3);
+        list.tambah(m4);
+        list.tambah(m5);
+
+        System.out.println("Data Mahasiswa sebelum sorting");
+        list.tampil();
+
+        System.out.println("\nData Mahasiswa setelah sorting desc berdasarkan ipk");
+        list.bubblesort();
+        list.tampil();
+    }    
+}
