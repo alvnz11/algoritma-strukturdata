f02f0274062c91d30293706451f6e24ea98c20e9
alvnz11 <you@example.com>
2024-05-13 12:56:40 +0700

    update: percobaan 2 jbs 10

--- Pertemuan13/jobsheet_10/src/doublelinkedlists/DoubleLinkedLists.java
+++ Pertemuan13/jobsheet_10/src/doublelinkedlists/DoubleLinkedLists.java
@@ -84,4 +84,58 @@ public class DoubleLinkedLists {
             System.out.println("\nBerhasil diisi");
         }
     }
+
+    public void removeFirst() throws Exception {
+        if(isEmpty()) {
+            throw new Exception("Linked List masih kosong, tidak dapat dihapus!");
+        } else if (size == 1) {
+            removeLast();
+        } else {
+            head = head.next;
+            head.prev = null;
+            size--;
+        }
+    }
+
+    public void removeLast() throws Exception {
+        if(isEmpty()) {
+            throw new Exception("Linked List masih kosong, tidak dapat dihapus!");
+        } else if (head.next == null) {
+            head = null;
+            size--;
+            return;
+        }
+        Node current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+        size--;
+    }
+
+    public void remove(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai indeks di luar batas");
+        } else if (index == 0) {
+            removeFirst();
+        } else {
+            Node current = head;
+            int i = 0;
+            while(i < index) {
+                current = current.next;
+                i++;
+            }
+            if (current.next == null) {
+                current.prev.next = null;
+            } else if (current.prev == null) {
+                current = current.next;
+                current.prev = null;
+                head = current;
+            } else {
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+            }
+            size--;
+        }
+    }
 }
--- Pertemuan13/jobsheet_10/src/doublelinkedlists/DoubleLinkedListsMain.java
+++ Pertemuan13/jobsheet_10/src/doublelinkedlists/DoubleLinkedListsMain.java
@@ -4,22 +4,23 @@ public class DoubleLinkedListsMain {
     public static void main(String[] args) throws Exception {
         DoubleLinkedLists dll = new DoubleLinkedLists();
 
+        dll.addLast(50);
+        dll.addLast(40);
+        dll.addLast(10);
+        dll.addLast(20);
         dll.print();
         System.out.println("Size: " + dll.size());
-        System.out.println("==============================");
-        dll.addFirst(3);
-        dll.addLast(4);
-        dll.addFirst(7);
+        System.out.println("===========================");
+        dll.removeFirst();
         dll.print();
         System.out.println("Size: " + dll.size());
-        System.out.println("==============================");
-        dll.add(40, 1);
+        System.out.println("===========================");
+        dll.removeLast();
         dll.print();
         System.out.println("Size: " + dll.size());
-        System.out.println("==============================");
-        dll.clear();
+        System.out.println("===========================");
+        dll.remove(1);
         dll.print();
         System.out.println("Size: " + dll.size());
-
     }
 }
